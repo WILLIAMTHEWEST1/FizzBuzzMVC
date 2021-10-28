84b14e783c44a19dd081af0f2e71431cfcbf2702
WILLIAMTHEWEST1 <90918613+WILLIAMTHEWEST1@users.noreply.github.com>
2021-10-28 09:36:23 -0500

    Add project files.

---
+++ FizzBuzzMVC/Controllers/FizzBuzzController.cs
@@ -0,0 +1,47 @@
+﻿using FizzBuzzMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FizzBuzzMVC.Controllers
+{
+    public class FizzBuzzController : Controller
+    {
+        public IActionResult FizzBuzz()
+        {
+            var model = new FizzBuzz();
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult FizzBuzz(FizzBuzz model)
+        {
+            for (var i = 1; i <= 100; i++)
+            {
+                if (i % model.FizzValue == 0 && i % model.BuzzValue == 0)
+                {
+                    model.Results.Add("FizzBuzz");
+                }
+                else if (i % model.FizzValue == 0)
+                {
+                    model.Results.Add("Fizz");
+                }
+                else if (i % model.BuzzValue == 0)
+                {
+                    model.Results.Add("Buzz");
+                }
+                else
+                {
+                    model.Results.Add(i.ToString());
+                }
+            }
+
+            var test = model.Results.Count;
+
+            return View(model);
+        }
+
+    }
+}
---
+++ FizzBuzzMVC/Models/FizzBuzz.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FizzBuzzMVC.Models
+{
+    public class FizzBuzz
+    {
+        //I need:
+
+        //Fizzvalue
+        public int FizzValue { get; set; } = 3;
+
+        //Buzzvalue
+        public int BuzzValue { get; set; } = 5;
+
+        //A way to output my results
+        public List<string> Results { get; set; } = new List<string>();
+    }
+
+}
